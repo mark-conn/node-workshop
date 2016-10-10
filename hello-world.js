e2f3c07368a7c0aa8d022ea4f2fa0afca1c5181b
Mark Conn <misterconn@gmail.com>
2016-10-10 17:55:32 +0000

    answer 2

--- hello-world.js
+++ hello-world.js
@@ -1,6 +1,14 @@
 //create pull
-console.log("Hello World!");
+//console.log("Hello World!");
 
-setTimeout(function() {
-    console.log("Hello World Again!");
-}, 10000);
+// setTimeout(function() {
+//     console.log("Hello World Again!");
+// }, 10000);
+
+function every10Seconds() {
+    console.log("Hello World!");
+     setTimeout(every10Seconds, 10000)
+    }
+
+setTimeout(every10Seconds, 10000);
+
